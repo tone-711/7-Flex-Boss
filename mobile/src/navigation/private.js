a0e81a3d28fb1a64481e6edd5ca570212ebafd59
Tone T. Thangsongcharoen <tone.thangsongcharoen@7-11.com>
2023-10-26 01:23:37 -0500

    mobile app scaffolding

--- mobile/src/navigation/private.js
+++ mobile/src/navigation/private.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 
 import Home from '../screens/Home';
 
