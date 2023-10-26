08dacf6ab96ae19fd7ac715255e484d9f628675c
Syed Asad <sasa3008@7-11.com>
2023-10-26 16:20:40 -0500

    commit

--- web/src/screens/Login.js
+++ web/src/screens/Login.js
@@ -1,33 +1,68 @@
-import '../style.css';
-import { Link } from "wouter";
-import Button from '@mui/material/Button';
-import { TextField } from '@mui/material';
-import { useLocation } from 'wouter';
+import "../style.css";
+import logo from '../logo.png';
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import { TextField } from "@mui/material";
+import { Link, useLocation } from "wouter";
 
 export default function Login() {
+  const [user, setUser] = useState("");
+  const [pass, setPass] = useState("");
   const {navigate} = useLocation();
 
-return (
-    <div className='Container'>
-        <div className='Login'>
-            <h2>7Flex</h2>
-           
-            <TextField id="outlined-basic" label="Enter Username" variant="outlined" style={{ width: 300, backgroundColor: '#FF9900', marginBottom: 15, borderRadius: 8 }}/>
-            <TextField id="outlined-basic" label="Enter Password" variant="outlined" style={{ width: 300, backgroundColor: '#FF9900', marginBottom: 15, borderRadius: 8 }}/>
+  return (
+    <div className="Container">
+      <div className="Login" style={{ justifyContent: 'center' }}>
+       
+        <img src={logo} className="Logo"/>
 
-            <Button 
-                variant="contained" 
-                style={{ width: 300, marginTop: 10, background: '#000000' }}
-                //onClick={() => canLogin()}
-            >Login</Button>
+        <TextField
+          value={user}
+          onChange={(e) => {
+            setUser(e.target.value);
+          }}
+          id="outlined-basic"
+          label="Enter Username"
+          variant="outlined"
+          style={{
+            width: 300,
+            backgroundColor: "#FF9900",
+            marginBottom: 15,
+            borderRadius: 8,
+          }}
+        />
+        <TextField
+          value={pass}
+          onChange={(e) => {
+            setPass(e.target.value);
+          }}
+          type="password"
+          id="outlined-basic"
+          label="Enter Password"
+          variant="outlined"
+          style={{
+            width: 300,
+            backgroundColor: "#FF9900",
+            marginBottom: 15,
+            borderRadius: 8,
+          }}
+        />
 
-    <Link href="/Register">
-       <a className="link" onClick={() => navigate('/register')}>Profile</a>
-     </Link>
-           
-        </div>
+        {!user || !pass ? null : (
+          <Button
+            variant="contained"
+            style={{ width: 300, marginTop: 10, background: "#000000" }}
+            onClick={() => localStorage.setItem('token', 'ABC')}
+          >
+            Login
+          </Button>
+        )}
+
+ <Link href="/Register">
+ <a className="link" onClick={() => navigate('/register')}>Profile</a>
+ </Link>
+      </div>
     </div>
-    
-    
   );
 }
+
--- web/src/screens/Shifts.js
+++ web/src/screens/Shifts.js
@@ -1,57 +1,108 @@
-import * as React from 'react';
-import ListSubheader from '@mui/material/ListSubheader';
-import List from '@mui/material/List';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Collapse from '@mui/material/Collapse';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import WorkIcon from '@mui/icons-material/Work';
-import CheckIcon from '@mui/icons-material/Check';
-import DraftsIcon from '@mui/icons-material/Drafts';
-import SendIcon from '@mui/icons-material/Send';
-import ExpandLess from '@mui/icons-material/ExpandLess';
-import ExpandMore from '@mui/icons-material/ExpandMore';
-import StarBorder from '@mui/icons-material/StarBorder';
+import * as React from "react";
+import Button from "@mui/material/Button";
 
 export default function Shifts() {
-  const [open, setOpen] = React.useState(true);
+  const mockData = [
+    {
+      hours: 8,
+      status: "unassigned",
+    },
+    {
+      hours: 8,
+      status: "assigned",
+    },
+    {
+      hours: 8,
+      status: "assigned",
+    },
+    {
+      hours: 6,
+      status: "assigned",
+    },
+  ];
 
-  const handleClick = () => {
-    setOpen(!open);
-  };
+  const unassignedTasks = mockData.filter((word) => word.status === 'unassigned');
+  const assignedTasks = mockData.filter((word) => word.status === 'assigned');
 
   return (
-    <div className='Container'>
-     <h2>Available Shifts</h2>
-
-     <div style={{ marginLeft: 90, marginRight: 90 }}>
-    <List
-      sx={{ width: '100%', bgcolor: 'background.paper' }}
-      component="nav"
-      aria-labelledby="Available Shifts"
+    <div
+      style={{
+        textAlign: "center",
+        flex: 1,
+        backgroundColor: "#6DC86E",
+        paddingBottom: 1000,
+        paddingTop: 10,
+      }}
     >
-      <ListItemButton onClick={handleClick}>
-        <ListItemIcon>
-        <WorkIcon />
-        </ListItemIcon>
-        <ListItemText primary="8 hours" />
-        {open ? <ExpandLess /> : <ExpandMore />}
-      </ListItemButton>
-      <Collapse in={open} timeout="auto" unmountOnExit>
-        <p>Shift details</p>
-        <List component="div" disablePadding>
-          <ListItemButton sx={{ pl: 4 }}>
-            <ListItemIcon>
-              <CheckIcon />
-            </ListItemIcon>
-            <ListItemText primary="Status" />
-          </ListItemButton>
-        </List>
-      </Collapse>
-    </List>
+      <h2 style={{ marginBottom: 80, fontSize: 45, color: 'white', marginTop: 30 }}>Shifts</h2>
+
+      <div style={{ marginBottom: 10 }}>
+        <div
+          style={{
+            height: 20,
+            width: 120,
+            background: "#FFCCCB",
+            marginLeft: 30,
+            borderRadius: 8,
+          }}
+        >
+          <p>UNassigned: {unassignedTasks.length}</p>
+        </div>
+
+        <div
+          style={{
+            height: 20,
+            width: 120,
+            background: "#b1dcd1",
+            marginLeft: 30,
+            borderRadius: 8,
+          }}
+        >
+          <p>assigned: {assignedTasks.length}</p>
+        </div>
+      </div>
+
+      <div style={{ marginLeft: 90, marginRight: 90 }}>
+        {mockData.map((x, i) => (
+          <Button variant="text" style={{ width: "100%", marginBottom: 30 }} onPress={console.log('button pressed')}>
+            <div
+              style={{
+                flex: 1,
+                backgroundColor:
+                  x.status !== "unassigned" ? "#b1dcd1" : "#FFCCCB",
+                borderRadius: 8,
+              }}
+            >
+              {/* <p style={{ textAlign: "start", paddingTop: 10, marginLeft: 10 }}>{i + 1+')'}</p> */}
+              <p
+                style={{
+                  textAlign: "start",
+                  marginLeft: 10,
+                  paddingTop: 15,
+                  fontSize: x.status === "unassigned" ? 20 : 16,
+                  fontWeight: x.status === "unassigned" ? "bold" : "normal",
+                  color: "black",
+                }}
+              >
+                Shift Hours: {x.hours}
+              </p>
+              <p
+                style={{
+                  textAlign: "start",
+                  marginLeft: 10,
+                  paddingBottom: 15,
+                  fontSize: x.status === "unassigned" ? 20 : 16,
+                  fontWeight: x.status === "unassigned" ? "bold" : "normal",
+                  color: "black",
+                }}
+              >
+                Shift Status: {x.status}
+              </p>
+            </div>
+          </Button>
+        ))}
+      </div>
     </div>
-    </div>
-   
   );
 }
+
